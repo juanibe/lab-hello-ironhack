3315a7473bf0257a171fb74ad577900451bd8ca2
Juan Ignacio Benito <juanignaciobenito@Juans-MacBook-Air.local>
2019-02-12 21:15:33 +0100

    without styles

---
+++ starter-code/src/App.js
@@ -0,0 +1,25 @@
+
+import React, { Component } from "react";
+
+import "./App.css";
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <div className="title">
+                    <h1> Say Hello to <br/>ReactJS </h1>
+                </div>
+                <div>
+                    <h3 className="title">You will learn a frontend framework from scratcj</h3>
+                    
+                    <button className="but">Awsome!</button>
+                </div>
+
+
+            </div>
+
+        );
+    }
+}
+
+export default App;
